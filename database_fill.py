7d2bdfb26b8eaf75d1ecacd7c75ef57658fe36ba
Dimitrios Minagias <dimitrisminagias@gmail.com>
2024-05-08 21:30:44 +0300

    recipies full

--- database_fill.py
+++ database_fill.py
@@ -163,6 +163,53 @@ def fill_Thematic_Unit():
         # The connection is automatically closed when exiting the 'with connection' block
         print("Database connection closed.")
 
+def fill_basic_ingredients():
+    connection = connect()
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO Ingredients (name, caloriesper100grams, Food_Category_name) VALUES (%s, %s, %s)"
+                with open('ingredients.csv', 'r') as file:
+                    csv_data = csv.reader(file)
+                    next(csv_data)  # Skip the header row
+                    for row in csv_data:
+                        cursor.execute(sql, (row[0], row[1], row[2]))
+                
+            connection.commit()
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # The connection is automatically closed when exiting the 'with connection' block
+        print("Database connection closed.")
+
+def fill_Recipies():
+    """df = pd.read_csv('recipies.csv', encoding='utf-8')
+    df = df[['National_Cuisine_name']]
+    df.drop_duplicates(inplace=True)
+    df.sort_values(by='National_Cuisine_name', inplace=True)
+    pd.DataFrame(df).to_csv('recipies_national_cuisines.csv', index=False)
+
+    df = pd.read_csv('national_cuisine.csv')
+    df = df[['name']]
+    df.sort_values(by='name', inplace=True)
+    pd.DataFrame(df).to_csv('national_cuisine.csv', index=False)"""
+    connection = connect()
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO Recipies (difficulty,name,description,preparation_time,cooking_time,fat,protein,carbs,cooking_or_pastry,basic_ingredient_name,National_Cuisine_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                with open('recipies.csv', 'r', encoding='utf-8') as file:
+                    csv_data = csv.reader(file)
+                    next(csv_data)  # Skip the header row
+                    for row in csv_data:
+                        cursor.execute(sql, (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+                
+            connection.commit()
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # The connection is automatically closed when exiting the 'with connection' block
+        print("Database connection closed.")
 
 
 fill_Professional_Expertise()
@@ -173,3 +220,5 @@ fill_Food_Category()
 fill_Equipment()
 fill_Etiquette()
 fill_Thematic_Unit()
+fill_basic_ingredients()
+fill_Recipies()
